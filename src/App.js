752ae1f9b4a238f06a193e2336b17d5d6f28d17d
Abdurrahmanewu <108385384+Abdurrahmanewu@users.noreply.github.com>
2022-10-12 08:56:43 +0600

    header & home component added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
-{
-  path:'/',
-  element:
-}
+    {
+      path: '/',
+    }
   ])
   return (
     <div className="App">
